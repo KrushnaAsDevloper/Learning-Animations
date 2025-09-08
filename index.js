68bfdfabd4a9f480ab5bf906b904d8f6d0767f6d
KrushnaAsDevloper <krushnawaghmare165@gmail.com>
2025-09-08 19:49:56 +0530

    learned about the svg formation and manipulation and make mini porject for practice

--- index.js
+++ index.js
@@ -58,3 +58,27 @@ gsap.to("#page3 h1", {
 
     }
 })
+
+// Gsap svg manipulation
+
+var path = `M 0 150 Q 0 150 1400 150`
+var finalPath = `M 0 150 Q 0 150 1400 150`
+
+let string = document.querySelector("#string")
+
+string.addEventListener("mousemove",(dets)=>{
+    path = `M 0 150 Q ${dets.x} ${dets.y} 1400 150`
+    gsap.to("svg path", {
+        attr:{d:path}, 
+        duration:0.3, 
+        ease:'power3.out'
+
+    })
+})
+string.addEventListener("mouseleave",()=>{
+    gsap.to("svg path", {
+        attr:{d:finalPath}, 
+        duration:1.5, 
+        ease:"elastic.out(1,0.2)"
+    })
+})
